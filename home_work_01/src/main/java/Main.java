ba3667d35a5d23f18bfe2ecda20177cb4af1f5c3
digitalfinesse <dizverst@gmail.com>
2018-02-08 23:34:29 +0300

    obfuscation commit

--- home_work_01/src/main/java/Main.java
+++ home_work_01/src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Home work #1\nHello, Maven!");
+        System.out.println("Home work #1\nHello, Obfuscation!");
     }
 }
